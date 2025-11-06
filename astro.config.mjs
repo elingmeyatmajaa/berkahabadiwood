c4aa0cbfb2244d7ee79f211df655a9ff3c8ce033
Eling Meyatmaja <eling.atma@gmail.com>
2025-11-06 22:50:08 +0700

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
-// import vercelServerless from "@astrojs/vercel/serverless";
-import vercel from "@astrojs/vercel/serverless";
+import vercel from "@astrojs/vercel/serverless"; // tetap pakai serverless
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
   output: "server",
-  adapter: vercel(),
+  adapter: vercel({
+    runtime: "nodejs20.x", // ✅ tambahkan baris ini
+  }),
 });
